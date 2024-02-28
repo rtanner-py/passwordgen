c16e94ce51968833d3bd60610add2070a79d706d
Rob Tanner <rob.tanner@gmail.com>
2024-02-28 08:55:37 +0000

    Updated readme.md Updated flags to include a "-d" default which - hides output - copies to clipboard - sets length to 20 - sets alphanum to true

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# passwordgen" 
+Application that generates a string of n random characters
+Can use letters and digits only, or letters, digits and punctation characters
+Optional flag to set length from the command line
+-- if this is not set, then the user will be prompted
+Optional flag to hide the output. If this is set to true and copy is set to false (default) then the program will exit.
+Optional flag to copy the output to the clipboard.
+
--- password.py
+++ password.py
@@ -17,15 +17,24 @@ def main() -> None:
     parser.add_argument("--hide", action="store_true", help="If set, will hide the output. Use in conjunction with -c, --copy. Set to false by default.")
     parser.add_argument("--length", type=int, default=0, help="Specify the desired password length")
     parser.add_argument("--alphanum", action="store_true", help="Limit character choice to letters and numbers only.")
+    parser.add_argument("-d", action="store_true", help="Sets usage to default settings, overwriting any other flags.")
     args = parser.parse_args()
 
-    options = {
-        "copy": args.copy,
-        "hide": args.hide,
-        "length": args.length,
-        "alphanum": args.alphanum
-    }
-
+    if not args.d:
+        options = {
+            "copy": args.copy,
+            "hide": args.hide,
+            "length": args.length,
+            "alphanum": args.alphanum
+        }
+    else:
+        options = {
+            "copy": True,
+            "hide": True,
+            "length": 20,
+            "alphanum": True
+        }
+    
     if not options["copy"] and options["hide"]:
         sys.exit("Hide set to true and copy set to false. Invalid combination. Exiting...")
 
